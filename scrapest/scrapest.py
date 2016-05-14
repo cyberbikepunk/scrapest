f5cf6e3349ae77b8f1e89c95f3b50cb10e8581a2
cyberbikepunk <loic.jounot@gmail.com>
2016-05-14 13:06:44 +0200

    Set up a simple logger and CLI parser.

--- scrapest/scrapest.py
+++ scrapest/scrapest.py
@@ -8,9 +8,19 @@ Usage:
 """
 
 from __future__ import print_function
-
 from docopt import docopt
+from logging import basicConfig, getLogger, DEBUG
 
-args = docopt(__doc__)
-print(args)
+
+basicConfig(format='[%(module)s] %(message)s', level=DEBUG)
+log = getLogger(__name__)
+
+
+def parse_args(args):
+    return {k.lower(): v for k, v in args.items()}
+
+
+if __name__ == '__main__':
+    cli_args = parse_args(docopt(__doc__))
+    log.debug('%s', dict(cli_args))
 
