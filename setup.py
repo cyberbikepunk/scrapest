ce639d2aa31515521eb232e0b8bbd07754aa93ac
cyberbikepunk <loic.jounot@gmail.com>
2016-05-14 11:14:05 +0200

    Add the boilerplate for tox tests.

---
+++ setup.py
@@ -0,0 +1,14 @@
+""" Set-up file for the scrapest """
+
+
+from distutils.core import setup
+
+
+setup(
+    name='Scrapest',
+    version='0.1',
+    description='Simple Scrapy testing framework',
+    author='cyberbikepunk',
+    author_email='loic@cyberpunk.bike',
+    packages=['scrapest'],
+)
