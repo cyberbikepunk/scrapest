75505d0201965d54b502d26d0dbf909575114992
cyberbikepunk <loic.jounot@gmail.com>
2016-05-14 18:42:37 +0200

    Write a function that dispatches the program based on the command line input.

--- scrapest/scrapest.py
+++ scrapest/scrapest.py
@@ -19,8 +19,31 @@ log = getLogger(__name__)
 def scrub_keys(raw_args):
     return {
         key.lower().lstrip('--').lstrip('<').rstrip('>'): value
-        for key, value in raw_args.items()
-    }
+        for key, value in kwargs.items()
+        }
+
+
+def build_scaffolding():
+    log.debug('Building test scaffolding')
+
+
+def cache_webpage(url):
+    log.debug('Caching %s', url)
+
+
+def uncache_webpage(url):
+    log.debug('Uncaching %s', url)
+
+
+def dispatch_command(init=False, add=False, remove=False, url=None):
+    if init:
+        build_scaffolding()
+    elif add:
+        cache_webpage(url)
+    elif remove:
+        uncache_webpage(url)
+    else:
+        print(__doc__)
 
 
 if __name__ == '__main__':
--- tests/test_scrapest.py
+++ tests/test_scrapest.py
@@ -1,7 +1,9 @@
 """ The test module (pytest + tox). """
 
+from mock import patch
+from pytest import mark
 
-from scrapest.scrapest import scrub_keys
+from scrapest.scrapest import scrub_keys, dispatch_command
 
 
 def test_scrub_keys():
@@ -9,3 +11,22 @@ def test_scrub_keys():
     args_in = dict(zip(['--foo', 'BAR', '<spam>', 'eggs'], values))
     args_out = dict(zip(['foo', 'bar', 'spam', 'eggs'], values))
     assert scrub_keys(args_in) == args_out
+
+
+dispatching_tests = [
+    ({'init': True, 'add': False, 'remove': False, 'url': None},
+     'build_scaffolding', [], {}),
+    ({'init': False, 'add': True, 'remove': False, 'url': 'http://dummy.com'},
+     'cache_webpage', ['http://dummy.com'], {}),
+    ({'init': False, 'add': False, 'remove': True, 'url': 'http://dummy.com'},
+     'uncache_webpage', ['http://dummy.com'], {})
+]
+
+
+# noinspection PyShadowingNames
+@mark.parametrize('params, function_name, args, kwargs', dispatching_tests)
+def test_dispatch_command(params, function_name, args, kwargs):
+    function_path = 'scrapest.scrapest.%s' % function_name
+    with patch(function_path, return_value=None) as function:
+        dispatch_command(**params)
+        function.assert_called_once_with(*args, **kwargs)
