3f40b769817563aa733c8d3c21ece71d0199797c
cyberbikepunk <loic.jounot@gmail.com>
2016-05-14 21:54:00 +0200

    Add the framework cache directory creation logic.

--- scrapest/scaffolding.py
+++ scrapest/scaffolding.py
@@ -2,9 +2,20 @@
 
 
 from logging import getLogger
+from os import getcwd, listdir, makedirs
+from os.path import abspath, join, isdir
+from sys import exit
 
 
 log = getLogger('scrapest.scaffolding')
+
+test_dir = 'tests'
+cache_dir = 'scrapest_cache'
+cache_path = join(test_dir, cache_dir)
+
+
+class ScaffoldingError(Exception):
+    pass
 
 
 def build_scaffolding():
@@ -17,4 +28,28 @@ def build_scaffolding():
     The function assumes a standard Scrapy file structure.
 
     """
+
+    current_dir = abspath(getcwd())
+    subdirectories = listdir(current_dir)
+
     log.debug('Building test scaffolding')
+    log.debug('Current directory is %s', current_dir)
+    log.debug('Subdirectories: %s', list(subdirectories))
+
+    # TODO: write pytests for the folder creation logic
+
+    if '.git' not in subdirectories:
+        raise ScaffoldingError('scrapest must be run from the root of the Scrapy project')
+
+    if isdir(cache_path):
+        log.info('Test scaffold already exists: exiting')
+        exit(0)
+
+    if isdir(test_dir):
+        log.debug('Using existing directory "%s"', test_dir)
+
+    try:
+        makedirs(cache_path)
+        log.debug('Created the cache %s', cache_path)
+    except IOError:
+        raise ScaffoldingError('I/O problem creating %s', cache_path)
